6f444ecd73773dc9a46b3afc757dcceaea596643
bakulevalena <bakuleva.lena@gmail.com>
2019-11-27 10:57:10 +0100

    Logging
    Logging

--- src/main/java/com/library/storage/service/BookProcessingService.java
+++ src/main/java/com/library/storage/service/BookProcessingService.java
@@ -5,7 +5,10 @@ import com.library.storage.entity.Author;
 import com.library.storage.entity.Book;
 import com.library.storage.repository.AuthorRepository;
 import com.library.storage.repository.BookRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -14,6 +17,7 @@ import java.util.List;
 @Repository
 public class BookProcessingService implements BookServices {
 
+    private static final Logger logger = LoggerFactory.getLogger(BookProcessingService.class);
     private BookRepository bookRepository;
     private AuthorRepository authorRepository;
 
@@ -24,6 +28,7 @@ public class BookProcessingService implements BookServices {
 
     @Override
     public List<BookDTO> getAllBooks() {
+        logger.debug("Getting all the books");
         List<Book> allBooks = bookRepository.findAll();
         List<BookDTO> resultList = new ArrayList<>(allBooks.size());
         for (Book book : allBooks) {
@@ -32,24 +37,34 @@ public class BookProcessingService implements BookServices {
             temp.setAuthor(book.getAuthor().getAuthorName());
             resultList.add(temp);
         }
+        if (allBooks.isEmpty()) {
+            logger.warn("Unable to get all books");
+        } else {
+            logger.debug("Get all the books are finished");
+        }
         return resultList;
     }
 
     @Override
     public BookDTO getBook(Long bookID) {
+        logger.debug("Getting the book by ID");
         BookDTO bookDTO = new BookDTO();
         Book book = bookRepository.findBookById(bookID);
         if (book != null) {
             bookDTO.setTitle(book.getTitle());
             bookDTO.setAuthor(book.getAuthor().getAuthorName());
         } else {
+            logger.warn("Unable to ger the book by ID {}", bookID);
             return null;
         }
+        logger.debug("The book {} is gotten", bookID);
         return bookDTO;
     }
 
     @Override
+    @Transactional
     public String proceedBook(Long authorID, BookDTO book) {
+        logger.debug("Saving the book");
         Book savedBook = new Book();
         Author author = authorRepository.findAuthorById(authorID);
         if (author != null) {
@@ -58,15 +73,18 @@ public class BookProcessingService implements BookServices {
             savedBook.setCreationDate(LocalDateTime.now());
             author.addBook(savedBook);
             bookRepository.save(savedBook);
+            logger.debug("The book of author {} is saved", authorID);
             return "Saved";
 
         } else {
+            logger.warn("Unable to save the book of author {}", authorID);
             return "This author is not exist";
         }
     }
 
     @Override
     public List<BookDTO> getBookByAuthor(Long authorId) {
+        logger.debug("Getting the book by the author ID");
         Author author = authorRepository.findAuthorById(authorId);
         if (author != null) {
             List<Book> allBooks = bookRepository.findBookByAuthor(author);
@@ -77,21 +95,26 @@ public class BookProcessingService implements BookServices {
                 temp.setAuthor(book.getAuthor().getAuthorName());
                 resultList.add(temp);
             }
+            logger.debug("The book of author {} is gotten", author);
             return resultList;
         } else {
+            logger.warn("Unable to ger book by author ID {}", authorId);
             return null;
         }
     }
 
     @Override
     public BookDTO getBookByAuthorAndId(Long authorId, Long bookId) {
+        logger.debug("Getting the book by the author ID and book ID");
         Book book = bookRepository.findBookById(bookId);
         if (book != null && book.getAuthor().getId().equals(authorId)) {
             BookDTO result = new BookDTO();
             result.setTitle(book.getTitle());
             result.setAuthor(book.getAuthor().getAuthorName());
+            logger.debug("The book {} of author {} is gotten", authorId, bookId);
             return result;
         } else {
+            logger.warn("Unable to get book by author ID {} and book ID {}", authorId, bookId);
             return null;
         }
     }
